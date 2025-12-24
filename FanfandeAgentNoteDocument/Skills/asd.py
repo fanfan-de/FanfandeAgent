2ea3e0999103ec22962c09d5a5a88263bccb1508
fanfan-de <54299348+fanfan-de@users.noreply.github.com>
2025-12-25 01:44:31 +0800

    12.25.1.44

---
+++ FanfandeAgentNoteDocument/Skills/asd.py
@@ -0,0 +1,55 @@
+import os
+from dotenv import load_dotenv
+from typing import  List, Optional, Dict, Any, Union
+from core.message.message import Message, LLMMessage
+from deepseek import DeepSeekAPI
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessage
+from rich import print as rprint
+
+
+#load env
+load_dotenv()
+
+class LLM:
+    #init
+    def __init__(self,model:str="deepseek-chat"):
+        self.model = model
+        self.client = OpenAI(
+            api_key=os.environ.get('DEEPSEEK_API_KEY'), 
+            base_url="https://api.deepseek.com"
+        )
+    #输入输出
+    def chat(self,messages:List[Message],tools:Optional[List[Dict]]=None)->Union[LLMMessage,ChatCompletionMessage]:
+        """
+        核心方法：发送消息列表，获取 AI 回复
+        :param messages: 历史消息列表
+        :param tools: (新增) 工具的 JSON Schema 列表
+        """
+        rprint(messages)
+        try:
+            ## 2. 构造 API 参数
+            params = {
+                "model": self.model,
+                "messages": [msg.to_openai_dict() for msg in messages],
+                #"messages": messages.model_dump(exclude_none=True),
+                "stream": False,
+            }
+
+             # 3. 如果有工具，则传入 tools 参数
+            if tools:
+                params["tools"] = tools
+                params["tool_choice"] = "auto" # 让模型自动决定是否调用
+
+            rprint(params)
+            # 发起请求
+            response = self.client.chat.completions.create(**params)
+            # 返回消息
+            return response
+        except Exception as e:
+            print(f"调用 LLM 出错: {e}")
+            # 打印最后一条消息以便调试
+            if messages:
+                rprint(f"Last Msg: {messages}")
+            return LLMMessage(content="对不起，我遇到了一点错误。")
+
